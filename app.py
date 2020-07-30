6802de3d45a7e4210fc3ee1538d9f30816d2ac47
kalii4747 <45517559+kalii4747@users.noreply.github.com>
2020-07-30 16:08:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', table="Wprowadź dane")
+    return render_template('index.html', text="Wprowadź dane")
 
 @app.route('/predict',methods=['POST'])
 def predict():
     
-        
+    table = ""
+    partyname = ""    
     try:
         party = request.form.get("party")
         key_word = request.form.get("keyword").lower()
@@ -21,14 +22,19 @@ def predict():
 
         if party == "0":
             model = pickle.load(open("models/PIS_15-iter_trained_model.sav", "rb"))
+            partyname = "PIS"
         elif party == "1":
             model = pickle.load(open("models/KO_15-iter_trained_model.sav", "rb"))
+            partyname = "KO"
         elif party == "2":
             model = pickle.load(open("models/LEWICA_15-iter_trained_model.sav", "rb"))
+            partyname = "LEWICA"
         elif party == "3":
             model = pickle.load(open("models/KONFEDERACJA_15-iter_trained_model.sav", "rb"))
+            partyname = "KONFEDERACJA"
         elif party == "4":
             model = pickle.load(open("models/PSL-KUKIZ_15-iter_trained_model.sav", "rb"))
+            partyname = "PSL-KUKIZ"
     
         most_similar = model.wv.most_similar(positive=key_word, topn=topn)
     
@@ -38,7 +44,7 @@ def predict():
     
         table = Results(results)
            
-        return render_template('index.html', table=table)
+        return render_template('index.html', table=table,  text="Najbardziej podobne słowa do {} dla {}".format(key_word, partyname))
     
     except:
         return render_template('index.html', table="Podane słowo nie występuje w słowniku"  )
