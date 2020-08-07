fd809711f290fb4287fdd2782d60ff5338cd3e70
kalii4747 <45517559+kalii4747@users.noreply.github.com>
2020-08-07 16:25:32 +0200

    Update plots.py

--- plots.py
+++ plots.py
@@ -80,7 +80,7 @@ class Full_plot():
         layout = go.Layout(
                 autosize=False,
                 width=1750,
-                height=650
+                height=700
                 )
         
         fig = go.Figure(data=data, layout=layout)
