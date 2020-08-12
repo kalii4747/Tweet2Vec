8cb6f51e039e8e43846da05393572f17664869ac
kalii4747 <45517559+kalii4747@users.noreply.github.com>
2020-08-12 13:29:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,25 +23,24 @@ def predict():
     table = ""
     partyname = ""    
     try:
+                
         party = request.form.get("party")
         key_word = request.form.get("keyword").lower()
         topn = int(request.form.get("topn"))
-
+        
         if party == "0":
-            model = pickle.load(open("models/PIS_15-iter_trained_model.sav", "rb"))
             partyname = "PIS"
         elif party == "1":
-            model = pickle.load(open("models/KO_15-iter_trained_model.sav", "rb"))
             partyname = "KO"
         elif party == "2":
-            model = pickle.load(open("models/LEWICA_15-iter_trained_model.sav", "rb"))
             partyname = "LEWICA"
         elif party == "3":
-            model = pickle.load(open("models/KONFEDERACJA_15-iter_trained_model.sav", "rb"))
             partyname = "KONFEDERACJA"
         elif party == "4":
-            model = pickle.load(open("models/PSL-KUKIZ_15-iter_trained_model.sav", "rb"))
             partyname = "PSL-KUKIZ"
+
+                    
+        model = pickle.load(open("models/" + partyname + "_15-iter_trained_model.sav", "rb"))
     
         most_similar = model.wv.most_similar(positive=key_word, topn=topn)
     
