4fad899df83c3e30df9b44e09ffbeffbc66ae822
kalii4747 <45517559+kalii4747@users.noreply.github.com>
2020-09-15 19:59:11 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -33,37 +33,56 @@ def tweet_gen():
 @app.route('/predict',methods=['POST'])
 def predict():
     
-    table = ""
-    partyname = ""    
+    table_pis = ""
+    table_ko = ""
+    table_lewica = ""
+    table_konfederacja = ""
+    table_pslkukiz = ""
+    
+ 
     try:
                 
-        party = request.form.get("party")
         key_word = request.form.get("keyword").lower().replace(" ", "")
         topn = int(request.form.get("topn"))
-        
-        if party == "0":
-            partyname = "PIS"
-        elif party == "1":
-            partyname = "KO"
-        elif party == "2":
-            partyname = "LEWICA"
-        elif party == "3":
-            partyname = "KONFEDERACJA"
-        elif party == "4":
-            partyname = "PSL-KUKIZ"
-
-                    
-        model = pickle.load(open("models/" + partyname + "_15-iter_trained_model.sav", "rb"))
+                                 
+        model_pis = pickle.load(open("models/PIS_15-iter_trained_model.sav", "rb"))
+        model_ko = pickle.load(open("models/KO_15-iter_trained_model.sav", "rb"))
+        model_lewica = pickle.load(open("models/LEWICA_15-iter_trained_model.sav", "rb"))
+        model_konfederacja = pickle.load(open("models/KONFEDERACJA_15-iter_trained_model.sav", "rb"))
+        model_pslkukiz = pickle.load(open("models/PSL-KUKIZ_15-iter_trained_model.sav", "rb"))
     
-        most_similar = model.wv.most_similar(positive=key_word, topn=topn)
+        most_similar_pis = model_pis.wv.most_similar(positive=key_word, topn=topn)
+        most_similar_ko = model_ko.wv.most_similar(positive=key_word, topn=topn)
+        most_similar_lewica = model_lewica.wv.most_similar(positive=key_word, topn=topn)
+        most_similar_konfederacja = model_konfederacja.wv.most_similar(positive=key_word, topn=topn)
+        most_similar_pslkukiz = model_pslkukiz.wv.most_similar(positive=key_word, topn=topn)
     
-        results = []
+        results_pis = []
         for i in range(topn):
-            results.append(Item(most_similar[i][0], round(most_similar[i][1],3)))
+            results_pis.append(Item(most_similar_pis[i][0], round(most_similar_pis[i][1],3)))
+        results_ko = []
+        for i in range(topn):
+            results_ko.append(Item(most_similar_ko[i][0], round(most_similar_ko[i][1],3)))
+        results_lewica = []
+        for i in range(topn):
+            results_lewica.append(Item(most_similar_lewica[i][0], round(most_similar_lewica[i][1],3)))
+        results_konfederacja = []
+        for i in range(topn):
+            results_konfederacja.append(Item(most_similar_konfederacja[i][0], round(most_similar_konfederacja[i][1],3)))
+        results_pslkukiz = []
+        for i in range(topn):
+            results_pslkukiz.append(Item(most_similar_pslkukiz[i][0], round(most_similar_pslkukiz[i][1],3)))
     
-        table = Results(results)
+        table_pis = Results(results_pis)
+        table_ko = Results(results_ko)
+        table_lewica = Results(results_lewica)
+        table_konfederacja = Results(results_konfederacja)
+        table_pslkukiz = Results(results_pslkukiz)
            
-        return render_template('most_similar.html', table=table,  party="Partia: {}".format(partyname), key_word="Słowo kluczowe: {}".format(key_word))
+        return render_template('most_similar.html', table_pis=table_pis, table_ko=table_ko, table_lewica=table_lewica, 
+                               table_konfederacja=table_konfederacja, table_pslkukiz=table_pslkukiz, 
+                                key_word="Słowo kluczowe: {}".format(key_word), pis="PiS", ko="KO", lewica="Lewica", 
+                                konfederacja="Konfederacja", pslkukiz="PSL-Kukiz")
     
     except:
         return render_template('most_similar.html', table="Podane słowo nie występuje w słowniku"  )
